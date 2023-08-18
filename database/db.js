54b7c77d0d9611e93ba00111c468e133c934a7ba
softabhi <99878096+softabhi@users.noreply.github.com>
2023-08-18 16:27:17 +0530

    starting server command change

--- database/db.js
+++ database/db.js
@@ -6,9 +6,10 @@ import mongoose from "mongoose";
 // app.use(express.json());
 // app.use(express.urlencoded());
 // app.use(cors());
+const DB = process.env.DATABASE
 
 const conectDb = ()=>{
-    const conn = mongoose.connect("mongodb+srv://kdhar499:DurLa1999mo@cluster0.rjxq1d0.mongodb.net/flipkartclonedb", {
+    const conn = mongoose.connect(DB, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     }).then(() => console.log("database connected")).catch((err) => console.log(err))
