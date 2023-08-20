dad48a3463c50f793ff96e7402d42999efee91c1
softabhi <99878096+softabhi@users.noreply.github.com>
2023-08-20 15:15:46 +0530

    fronted url merg in server file

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser())
 app.use(express.urlencoded({extended:false}));
-app.use(cors({credentials:true, origin:"http://localhost:3000"}));
+app.use(cors({credentials:true, origin:"https://verdant-queijadas-c89883.netlify.app"}));
 app.use('/uploads',express.static('uploads'))
 
 app.use('/api/v1/', users)
