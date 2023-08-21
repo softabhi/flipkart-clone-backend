10c5759bfc27a2d173d751d4211bf866fb8dab03
softabhi <99878096+softabhi@users.noreply.github.com>
2023-08-21 14:02:53 +0530

    fronted url remove in server file

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser())
 app.use(express.urlencoded({extended:false}));
-app.use(cors());
+app.use(cors({credentials:true}));
 app.use('/uploads',express.static('uploads'))
 
 app.use('/api/v1/', users)
